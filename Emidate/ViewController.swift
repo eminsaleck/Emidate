b0c9dddfbcbcf44d2c0cbc78b104ab6058654c8f
username <email@blahblah.com>
2022-10-03 16:52:09 +0300

    com

--- Emidate/ViewController.swift
+++ Emidate/ViewController.swift
@@ -11,7 +11,9 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        /*Do any additional setup after loading the view.
+         here will be app
+         */
     }
 
 
